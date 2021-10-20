d5382577a5453ab7f7939bba763d7b4722ea21c9
dbrunken <david.brunken@startmail.com>
2021-10-20 16:31:44 -0700

    still working on calendar

--- PlanDirectApp/views.py
+++ PlanDirectApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.utils import timezone
+from datetime import datetime
+import pytz
 
 from .models import Entry, toDo
 
@@ -33,3 +36,25 @@ def new_task(request):
         toDo.objects.create(
             new_task=task
         )
+    def get_tasks(request):
+        task_objects = toDo.objects.all()
+        tasks = []
+        for task_object in task_objects:
+            task = {
+                'id' : task_object.id,
+                'task' : task_object.task,
+                'date_added' : task_object.date_added,
+                'completed' : task_object.completed
+            }
+            tasks.append(task)
+        data = {
+            'tasks' : tasks
+        }
+        return JsonResponse(data)
+        
+
+def covert_localtime(utctime):
+    fmt = '%Y-%M-%D %T'
+    utc = utctime.replace(tzinfo=pytz.UTC)
+    localtz = utc.astimezone(timezone.get_current_timezone())
+    return localtz.strftime(fmt)
