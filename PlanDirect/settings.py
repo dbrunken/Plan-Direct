546a9cbc68b8db8b02a45ad4d93162d8e4bd9858
dbrunken <david.brunken@startmail.com>
2021-11-02 10:48:29 -0700

    more heroku

--- PlanDirect/settings.py
+++ PlanDirect/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn PlanDirect.wsgi
+release: python manage.py migrate
